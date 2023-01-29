6221620f4fd49dcb4de9596ad65d7f0630720f43
iniagbo <mzeepositive@gmail.com>
2023-01-29 09:15:33 -0800

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -28,6 +28,8 @@ class Square:
     def size(self):
         """
         size getter. Handle size errors
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -39,3 +41,15 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def my_print(self):
+        """
+        Print a square with the character #
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(self.__size):
+                for column in range(self.__size):
+                    print("#", end="")
+                print()
