47ed064eef8a62b1c64b598caff1e5078352eb33
iniagbo <mzeepositive@gmail.com>
2023-01-29 09:59:21 -0800

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,7 +18,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        self.__size = size
+        self.position = position
 
     def area(self):
         """
