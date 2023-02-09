9099a29841ac4c98bf8a968f9cc4eac81dac726c
iniagbo <mzeepositive@gmail.com>
2023-02-09 03:09:27 -0800

    9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,27 +1,53 @@
 #!/usr/bin/python3
+"""
+This is module 9-rectangle
+
+This module contains:
+-one class
+-two functions
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-"""
-Rectangle class module.
-
-Define Rectangle class.
-"""
-
-
 class Rectangle(BaseGeometry):
-    """Define a Rectangle."""
+    """
+    inherits class from BaseGeometry (7-base_geometry)
+
+    Instantiation with width and height:
+    def __init__(self, width, height):
+    width and height must be private. No getter or setter
+    width and height must be positive integers, validated by integer_validator
+
+    Args:
+    width
+    height
+    Returns:
+    area of shape
+    """
 
     def __init__(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        """
+        private instantiation
+        """
         self.__width = width
         self.__height = height
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)
 
     def area(self):
-        """Returns the area of the rectangle."""
-        return self.__width * self.__height
+        """
+        returns the area
+        """
+        return (self.__width * self.__height)
 
     def __str__(self):
-        return "[{}] {}/{}".format(type(self).__name__,
-                                   self.__width, self.__height)
+        """
+        instance method for string - print class name with
+        following rectangle description: [Rectangle] <width>/<height>
+        """
+        return ("[{:s}] {:d}/{:d}".
+                format(self.__class__.__name__,
+                       self.__width,
+                       self.__height))
